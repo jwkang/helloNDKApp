f5156f8577f9415f96a4925b160c9d3e9a4ade67
jongwon kang <krnomad@gmail.com>
2019-03-24 12:31:57 +0900

    add jni source file in Android.mk

--- ndk/jni/Android.mk
+++ ndk/jni/Android.mk
@@ -11,12 +11,12 @@ LOCAL_CPPFLAGS += -O0 -pthread -fexceptions
 LOCAL_LDLIBS := -llog
 
 LOCAL_SRC_FILES := \
-	jni_common/jwJNI.cpp
+	jni_common/jwJNI.cpp \
+	com_app_example_jwkang2_hello_model_HTunerHal.cpp
 
+LOCAL_C_INCLUDES := $(subst ${ANDROID}/,,$(LOCAL_C_INCLUDES))
 LOCAL_C_INCLUDES += \
 	$(LOCAL_PATH)/include \
 	$(JNI_H_INCLUDE)
-
-LOCAL_C_INCLUDES := $(subst ${ANDROID}/,,$(LOCAL_C_INCLUDES))
 
 include $(BUILD_SHARED_LIBRARY)
